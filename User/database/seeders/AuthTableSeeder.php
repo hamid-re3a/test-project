ad8508b88fa5a4dca070cec081aeb6ec1b6310ef
hamid <hamire3a@gmail.com>
2022-01-09 19:48:03 +0330

    force production

--- User/database/seeders/AuthTableSeeder.php
+++ User/database/seeders/AuthTableSeeder.php
@@ -43,48 +43,7 @@ class AuthTableSeeder extends Seeder
 
             $admin->assignRole([USER_ROLE_SUPER_ADMIN,USER_ROLE_CLIENT]);
         }
-        if (!in_array(app()->environment(), ['production'])) {
-            if (!User::query()->where('email', 'companystaging@gmail.com')->exists()) {
-                $global = User::whereUsername('johny')->first();
-                if (!$global) {
-                    $global = User::factory()->create([
-                        'username' => 'johny',
-                    ]);
-                    $global->update([
-                        'member_id' => '2000',
-                        'email' => 'companystaging@gmail.com',
-                        'password' => 'password',
-                        'first_name' => 'John',
-                        'last_name' => 'Due',
-                        'username' => 'johny',
-                        'email_verified_at' => now()
-                    ]);
-                }
 
-                $global->assignRole([USER_ROLE_SUPER_ADMIN,USER_ROLE_CLIENT]);
-            }
-            if (!User::query()->where('email', 'customer@yopmail.com')->exists()) {
-                $global = User::whereUsername('george')->first();
-                if (!$global) {
-                    $global = User::factory()->create([
-                        'username' => 'george',
-                    ]);
-                    $global->update([
-                        'member_id' => '3000',
-                        'email' => 'customer@yopmail.com',
-
-                        'password' => 'password',
-                        'first_name' => 'George',
-                        'last_name' => 'W Father',
-                        'username' => 'george',
-                        'email_verified_at' => now()
-                    ]);
-                }
-
-                $global->assignRole([USER_ROLE_CLIENT]);
-            }
-
-        }
 
     }
 }
